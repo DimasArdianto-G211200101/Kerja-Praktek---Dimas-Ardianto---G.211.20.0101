2250a073ae8ec45f677239f631317912b3ee2016
“Dimas <dimardians016@gmail.com>
2024-01-06 16:29:04 +0700

    Pengaduan Masyarakat

--- PENGKAT-Pengaduan-Masyarakat/resources/views/includes/masyarakat/footer.blade.php
+++
@@ -1,4 +0,0 @@
-<footer class="mt-3 p-3 text-center bg-white text-sm text-gray-600 dark:text-gray-400 dark:bg-gray-800">
-  © {{ now()->year }} PENGKAT | By
-  <a href="https://diskominfo.semarangkota.go.id/" class="text-blue-500" target="_blank">Dimas Ardianto</a>
-</footer>
